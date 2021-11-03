a2b1d1bd220961d0381815eb095596aca300172b
lihaiyong <hyli@aceinna.com>
2021-11-03 13:16:26 +0800

    fix:ntrip rtcm data log saved

--- src/aceinna/devices/base/rtk_provider_base.py
+++ src/aceinna/devices/base/rtk_provider_base.py
@@ -60,6 +60,7 @@ class RTKProviderBase(OpenDeviceBase):
         self.debug_logf = None
         self.rtcm_logf = None
         self.debug_c_f = None
+        self.ntrip_rtcm_logf = None
         self.enable_data_log = False
         self.is_app_matched = False
         self.ntrip_client_enable = False
@@ -230,8 +231,11 @@ class RTKProviderBase(OpenDeviceBase):
         self.ntrip_client.run()
 
     def handle_rtcm_data_parsed(self, data):
+        bytes_data = bytearray(data)
         if self.communicator.can_write() and not self.is_upgrading:
-            self.communicator.write(data)
+            self.communicator.write(bytes_data)
+
+        self.ntrip_rtcm_logf.write(bytes_data)
 
     def build_connected_serial_port_info(self):
         if not self.communicator.serial_port:
@@ -284,6 +288,9 @@ class RTKProviderBase(OpenDeviceBase):
 
         # start ntrip client
         if self.properties["initial"].__contains__("ntrip") and not self.ntrip_client and not self.is_in_bootloader:
+            self.ntrip_rtcm_logf = open(os.path.join(self.rtk_log_file_name, 'ntrip_rtcm_{0}.bin'.format(
+                formatted_file_time)), "wb")
+
             thead = threading.Thread(target=self.ntrip_client_thread)
             thead.start()
 

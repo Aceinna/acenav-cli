49dbdf9d8b9ad7bb22caf6af8ae6f92c2786005c
lihaiyong <hyli@aceinna.com>
2022-03-07 15:15:06 +0800

    fix:The serial port baud rate of STA9100 SDK firmware upgraded is compatible with 230400bps and 115200, which is distinguished by  the firmware version of the 0xcc01 message.

--- tests/test_ins401_upgrade.py
+++ tests/test_ins401_upgrade.py
@@ -33,9 +33,8 @@ def handle_discovered(device_provider):
             print('loop_upgrade_cnt: %d' % loop_upgrade_cnt)
             print('loop_upgrade_cnt: %d' % loop_upgrade_cnt, file = upgrade_log_file, flush = True)
 
-        device_provider.upgrade_framework(['upgrade', './INS401_28.02.05.bin'])
-
-        if loop_upgrade_cnt == 300:
+        device_provider.upgrade_framework(['upgrade', './INS401_28.03a.bin'])
+        if loop_upgrade_cnt == 200:
             os._exit()
 
         time.sleep(5)
